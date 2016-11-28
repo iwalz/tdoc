c1f110c52f17080af838391555560ac9175549d6
Ingo Walz <ingo.walz@googlemail.com>
2016-11-28 01:36:54 +0100

    Additional unit test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -189,6 +189,33 @@ foo"`
 	}
 }
 
+func TestAliasDoubleQuoteMultilineIdentifier(t *testing.T) {
+	input := `actor "test
+foo" as foo`
+
+	tests := []struct {
+		extectedType    int
+		expectedLiteral string
+	}{
+		{COMPONENT, "actor"},
+		{IDENTIFIER, "test\nfoo"},
+		{ALIAS, "as"},
+		{TEXT, "foo"},
+	}
+
+	l := NewLexer(input)
+	for i, tt := range tests {
+		lval := &TdocSymType{}
+		tok := l.Lex(lval)
+		if tok != tt.extectedType {
+			t.Fatalf("test[%d] - wrong type, expected=%q, got=%q", i, tt.extectedType, tok)
+		}
+		if lval.val != tt.expectedLiteral {
+			t.Fatalf("test[%d] - wrong value, expected=%q, got=%q", i, tt.expectedLiteral, lval.val)
+		}
+	}
+}
+
 func TestSimpleAliasDeclaration(t *testing.T) {
 	input := `actor test as foo`
 
