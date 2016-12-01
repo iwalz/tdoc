639c8155b9fb7816d3f7b5778171318089893484
Ingo Walz <ingo.walz@googlemail.com>
2016-12-02 00:12:59 +0100

    Added some parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,9 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSimpleParser(t *testing.T) {
+func TestSimpleComponent(t *testing.T) {
 	p := &TdocParserImpl{}
 	p.Parse(NewLexer("cloud foo"))
-	foo := p.AST()
-	assert.Equal(t, "*parser.ProgramNode", reflect.TypeOf(foo).String())
+	ast := p.AST()
+	assert.Equal(t, "*parser.ProgramNode", reflect.TypeOf(ast).String())
+	d := ast.Front()
+	assert.Equal(t, "*parser.DefaultNode", reflect.TypeOf(d).String())
+	c := d.Front()
+	assert.Equal(t, "*parser.ComponentNode", reflect.TypeOf(c).String())
+	assert.Equal(t, "foo", c.(*ComponentNode).Identifier)
+	assert.Equal(t, "cloud", c.(*ComponentNode).Component)
+}
+
+func TestAliasComponent(t *testing.T) {
+	p := &TdocParserImpl{}
+	p.Parse(NewLexer("cloud foo as bar"))
+	ast := p.AST()
+	assert.Equal(t, "*parser.ProgramNode", reflect.TypeOf(ast).String())
+	d := ast.Front()
+	assert.Equal(t, "*parser.DefaultNode", reflect.TypeOf(d).String())
+	a := d.Front()
+	assert.Equal(t, "*parser.AliasNode", reflect.TypeOf(a).String())
+	assert.Equal(t, "bar", a.(*AliasNode).Alias)
+	c := a.Front()
+	assert.Equal(t, "foo", c.(*ComponentNode).Identifier)
+	assert.Equal(t, "cloud", c.(*ComponentNode).Component)
 }
